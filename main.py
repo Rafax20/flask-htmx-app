db1a2386c0a0846ceab5eba2e5ab72c80281d858
Rafax20 <159099789+Rafax20@users.noreply.github.com>
2025-07-25 19:53:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 from correo import enviar_bienvenida
 import os
@@ -13,6 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 with app.app_context():
     try:
